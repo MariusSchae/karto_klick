51a98f01334dd82ad7cfb6f768e2c20528081894
MariusSchae <93968982+MariusSchae@users.noreply.github.com>
2021-11-30 13:12:56 +0100

    Start/endpoint select

--- Karto_klick.py
+++ Karto_klick.py
@@ -21,16 +21,39 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
+from qgis.PyQt.QtCore import Qt, QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
+from qgis.gui import QgsMapTool,QgsMapToolPan
 
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
 from .Karto_klick_dialog import KartoKlickDialog
+from qgis.core import *
 import os.path
 
+
+class SendPointToolCoordinates(QgsMapTool):
+    """ Catches the coordinates from a click on a layer and displays them in a UI element
+    """
+    def __init__(self, canvas, window, label):
+        """ Constructor.
+        """
+        QgsMapTool.__init__(self, canvas)
+        self.canvas = canvas
+        self.window = window # where we'll show the coordinates
+        self.label = label
+        self.setCursor(Qt.CrossCursor)
+        window.hide()
+    """ Wird aufgerufen, wenn mit der Maus in den Map-Canvas geklickt wird.
+    """
+    def canvasReleaseEvent(self, event):
+        point = self.toMapCoordinates(event.pos())
+        self.label.setText(str(point.x())+", "+str(point.y()))
+        self.canvas.setMapTool(QgsMapToolPan(self.canvas))
+        self.window.show()
+        self.setCursor(Qt.ArrowCursor)
 
 class KartoKlick:
     """QGIS Plugin Implementation."""
@@ -47,40 +70,14 @@ class KartoKlick:
         self.iface = iface
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'KartoKlick_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            QCoreApplication.installTranslator(self.translator)
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&KartoKlick')
+        self.menu = u'&KartoKlick'
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('KartoKlick', message)
 
 
     def add_action(
@@ -163,7 +160,7 @@ class KartoKlick:
         icon_path = ':/plugins/Karto_klick/icon.png'
         self.add_action(
             icon_path,
-            text=self.tr(u'Karto Klicker'),
+            text=u'Karto Klicker',
             callback=self.run,
             parent=self.iface.mainWindow())
 
@@ -175,13 +172,22 @@ class KartoKlick:
         """Removes the plugin menu item and icon from QGIS GUI."""
         for action in self.actions:
             self.iface.removePluginMenu(
-                self.tr(u'&KartoKlick'),
+                u'&KartoKlick',
                 action)
             self.iface.removeToolBarIcon(action)
 
 
     def run(self):
         """Run method that performs all the real work"""
+        canvas = self.iface.mapCanvas()
+        def getCoordinate1():
+            send_point_tool_coordinates= SendPointToolCoordinates(canvas,self.dlg, self.dlg.coordinate1)
+            canvas.setMapTool(send_point_tool_coordinates)
+
+
+        def getCoordinate2():
+            send_point_tool_coordinates= SendPointToolCoordinates(canvas,self.dlg, self.dlg.coordinate2)
+            canvas.setMapTool(send_point_tool_coordinates)
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
@@ -189,12 +195,29 @@ class KartoKlick:
             self.first_start = False
             self.dlg = KartoKlickDialog()
 
+
+            self.dlg.coordinateSelect1.clicked.connect(getCoordinate1)
+
+            self.dlg.coordinateSelect2.clicked.connect(getCoordinate2)
+
+
         # show the dialog
         self.dlg.show()
+
+
         # Run the dialog event loop
         result = self.dlg.exec_()
         # See if OK was pressed
         if result:
             # Do something useful here - delete the line containing pass and
             # substitute with your code.
+            point = self.dlg.coordinate1.text()
+
+            if point == "":
+                print("leer")
+            else:
+                print("startpunkt="+point)
+
+
+
             pass
